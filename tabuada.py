d3fe2caf011004bc56eb3a9af868f160f45bba39
Lucas Matsuse <lucas.matsuse@engenharia.ufjf.br>
2023-10-25 20:16:48 -0300

    mudança na formatação do texto

--- tabuada.py
+++ tabuada.py
@@ -1,14 +1,24 @@
 #!usr/bin/env python
 """Imprime as tabuadas de 1 à 10"""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Lucas Matsuse"
 
 base = list(range(1,11))
+template = """
+---tabuada do {numero} ---
+
+         {operacao}         
+
+###################
+"""
 
 for i in base:
-    print(f"Tabuada do :{i}")
+    texto = f"Tabuada do {i}"
+    cabecalho = "{:-^20}".format(texto)
+    print(cabecalho)
+    print()
     for j in base:
-        print(f"{i} X {j} = {i*j}")
-        
-    print(30*"-")
-    
+        operacao = f"{i} X {j} = {i*j}"
+        print("{:-^20}".format(operacao))
+    print()
+    print(20*"#")
