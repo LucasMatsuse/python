4abe82198124dec39bd29608b50a876d56062381
Lucas Matsuse <lucas.matsuse@engenharia.ufjf.br>
2023-10-26 20:13:21 -0300

    sys.argv ,CLI argument, input

--- hello2.py
+++ hello2.py
@@ -12,21 +12,43 @@ execução:
     SET LANG=pt_BR
     ou
     export LANG=pt_BR
-
+ou informe atraves do CLI argument '--LANG=pt_BR'. Caso contrário o programa
+pedirá para o usuário informar com o input.
 Execução:
 
-    python hello.py
+    python hello2.py
     ou
-    ./hello.py
+    ./hello2.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Lucas Matsuse"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG","en_US")[:5]
+arguments = {
+    "lang":None,
+    "count":1
+}
 
+for arg in sys.argv[1:]:
+    key, value = arg.split("=")
+    key = key.lstrip('-').strip().lower()
+    value = value.strip()
+    if key not in arguments.keys():
+        print(f"Invalid Option '{key}'")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments['lang']
+if arguments['lang'] is None: 
+    current_lang = os.getenv("LANG")
+    if current_language is None:
+        current_language = input(
+            "Choose a language "
+        )
+current_language = current_language[:5]
 """
 if current_language == "pt_BR":
     msg = "Olá, Mundo!"
@@ -42,4 +64,4 @@ msg ={
     "fr_FR":"Bonjour Monde!",
     "es_SP":"Hola, Mundo!"
 }
-print(msg[current_language])
+print(msg[current_language] * int(arguments['count']))
