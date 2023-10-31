2588d3c13885f877ecb8b739293974ff9ccee285
Lucas Matsuse <lucas.matsuse@engenharia.ufjf.br>
2023-10-31 15:38:14 -0300

    tratamento de erro EAFP

--- prefixcalc.py
+++ prefixcalc.py
@@ -22,7 +22,7 @@ n1: 5
 n2: 2
 7
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 import sys
 import os
@@ -30,37 +30,45 @@ from datetime import datetime
 
 
 
-if len(sys.argv) >= 4:
+if len(sys.argv) == 4:
     func = sys.argv[1].strip('-').lower().strip()
     n1 = float(sys.argv[2].strip('-'))
     n2 = float(sys.argv[3].strip('-'))
 else:
+    print('Você digitou uma quantidade inesperada de argumentos.')
     func = input('digte a operação: ')
     n1 = float(input('digite o primeiro número: '))
     n2 = float(input('digite o segundo número: '))
 
 
-divzero = (func=='div') and (n2==0)
-if divzero:
+try:
+    resultado = {
+        'sum': n1 + n2,
+        'sub': n1 - n2,
+        'mul': n1 * n2,
+        'div': n1 / n2
+    }
+    print(resultado[func])
+
+except ZeroDivisionError as e:
+    print(str(e))
     print('Não é possível fazer divisões por 0.')
     sys.exit(1)
-resultado = {
-    'erro': 'Você digitou uma operação que esse programa não resolve, ou'
-            ' não passou a operação como primeiro argumento.',
-    'sum': n1 + n2,
-    'sub': n1 - n2,
-    'mul': n1 * n2,
-    'div': n1 / n2
-}
-if func not in resultado.keys():
-    func = 'erro'
+
+except KeyError as e:
+    print(str(e))
+    print('A operação indicada não é válida.')
+    sys.exit(1)
+
 
 path = os.curdir
 filepath = os.path.join(path,'prefixcalc.log')
 timestamp = datetime.now().isoformat()
 user = os.getenv('USER','anonymous')
 
-with open(filepath,'a') as file_:
-    file_.write(f'{timestamp} - {user} - {func}, {n1}, {n2} = {resultado[func]}.\n')
-
-print(resultado[func])
+try:
+    with open(filepath,'a') as file_:
+        file_.write(f'{timestamp} - {user} - {func}, {n1}, {n2} = {resultado[func]}.\n')
+except PermissionError as e:
+    print(str(e))
+    sys.exit(1)
