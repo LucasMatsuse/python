c24fa5780c723ebe51c41d1b87bfc6bec99c25f5
Lucas Matsuse <lucas.matsuse@engenharia.ufjf.br>
2023-10-31 18:12:33 -0300

    erros de digitação

--- logs.py
+++ logs.py
@@ -9,26 +9,32 @@ critical
 """
 import logging
 import os
+try:
+    1/0
+except ZeroDivisionError as e:
+    logging.error('%s',str(e))
+
+
 from logging import handlers
 #boilerplate
 #TODO: usar função
 #TODO: usar lib
 
-log_level = os.getenv("LOG_LEVEL","WARNIG").upper()
+log_level = os.getenv("LOG_LEVEL","WARNING").upper()
 log = logging.Logger('nome',log_level)
 #ch = logging.StreamHandler()
-#ch.setlevel(log_level)
-fh = handlers.RottatingFileHandler(
+#ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
     'meulog.log',
     maxBytes =10**6,
     backupCount = 10 
 )
-fh.setlevel(log_level)
+fh.setLevel(log_level)
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s '
     'l:%(lineno)d f:%(filename)s: %(message)s'
 )
-#ch.SetFormatter(fmt)
-fh.SetFormatter(fmt)
+#ch.setFormatter(fmt)
+fh.setFormatter(fmt)
 #log.addHandler(ch)
 log.addHandler(fh)
