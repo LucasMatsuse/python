30df18df127867c7d83ef1e907219e9fde1bc115
Lucas Matsuse <lucas.matsuse@engenharia.ufjf.br>
2023-10-31 17:34:21 -0300

    file handler logs

--- logs.py
+++ logs.py
@@ -9,18 +9,26 @@ critical
 """
 import logging
 import os
-
+from logging import handlers
 #boilerplate
 #TODO: usar função
 #TODO: usar lib
 
 log_level = os.getenv("LOG_LEVEL","WARNIG").upper()
 log = logging.Logger('nome',log_level)
-ch = logging.StreamHandler()
-ch.setlevel(log_level)
+#ch = logging.StreamHandler()
+#ch.setlevel(log_level)
+fh = handlers.RottatingFileHandler(
+    'meulog.log',
+    maxBytes =10**6,
+    backupCount = 10 
+)
+fh.setlevel(log_level)
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s '
     'l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.SetFormatter(fmt)
-log.addHandler(ch)
+#ch.SetFormatter(fmt)
+fh.SetFormatter(fmt)
+#log.addHandler(ch)
+log.addHandler(fh)
